a375739dbb3a16425a3c11af6322d2291546d37e
Jesse Bond <jjb315@lehigh.edu>
2013-02-19 22:20:53 -0500

    fixed packing? maybe

--- Roguish/src/com/me/Roguish/Model/Level.java
+++ Roguish/src/com/me/Roguish/Model/Level.java
@@ -11,7 +11,7 @@ public class Level{
 	private Array<Entity> entities = new Array<Entity>();
 	
     public TiledMap map = TiledLoader.createMap(Gdx.files.internal("data/test.tmx"));
-    public TileAtlas atlas = new TileAtlas(map, Gdx.files.internal("data/levels/pack"));
+    public TileAtlas atlas = new TileAtlas(map, Gdx.files.internal("data/"));
 
     
 	public Level(){
