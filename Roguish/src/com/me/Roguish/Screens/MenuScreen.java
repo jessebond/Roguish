8463fd11e7f613566ad1cfa58d8352863fbced1a
Curtis Giddings <curtis.giddings@gmail.com>
2013-02-19 17:06:47 -0800

    Fixed Menu & Splash Screens
    Still need to implement GameScreen constructor

--- Roguish/src/com/me/Roguish/Screens/MenuScreen.java
+++ Roguish/src/com/me/Roguish/Screens/MenuScreen.java
@@ -1,31 +1,48 @@
 package com.me.Roguish.Screens;
-import com.badlogic.gdx.scenes.scene2d.*;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.me.Roguish.Roguish;
 
 public class MenuScreen extends AbstractScreen {
+	TextureRegion title;
+	SpriteBatch batch;
+	float time = 0;
 	
 	public MenuScreen(Roguish game){
 		super(game);
 	}
 	
 	@Override
-	public void show(){
-		super.show();
-		Table table = super.getTable();
-		table.add("Welcome to Roguish").spaceBottom(50);
-		table.row();
-		
-		//Setup Start Game button
-		TextButton startGameButton = new TextButton("Start Match", getSkin());
-		startGameButton.addListener(new DefaultActorListener(){
-			@Override
-			public void touchUp(ActorEvent event, float x, float y, int pointer, int button){
-				super.touchUp(event, x, y, pointer, button);
-				game.setScreen(new StartGameSCreen(game));
+	public void show () {
+		title = new TextureRegion(new Texture(Gdx.files.internal("data/title.png")), 0, 0, 480, 320);
+		batch = new SpriteBatch();
+		batch.getProjectionMatrix().setToOrtho2D(0, 0, 480, 320);
+	}
+
+	@Override
+	public void render (float delta) {
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(title, 0, 0);
+		batch.end();
+
+		time += delta;
+		if (time > 1) {
+			if (Gdx.input.isKeyPressed(Keys.ANY_KEY) || Gdx.input.justTouched()) {
+				game.setScreen(new GameScreen(game));
 			}
-		});
-		
+		}
+	}
+
+	@Override
+	public void hide () {
+		Gdx.app.debug("Roguish", "dispose main menu");
+		batch.dispose();
+		title.getTexture().dispose();
 	}
 }
