0728f7758d8f7b48c423975fec7e1d0714d55629
Curtis Giddings <curtis.giddings@gmail.com>
2013-02-20 16:48:22 -0800

    Added semiworking start button
    Button complete with engineering art. Hit Detection coordinates need
    some work.

--- Roguish/src/com/me/Roguish/Screens/MenuScreen.java
+++ Roguish/src/com/me/Roguish/Screens/MenuScreen.java
@@ -2,14 +2,16 @@ package com.me.Roguish.Screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.me.Roguish.Roguish;
 
-public class MenuScreen extends AbstractScreen {
+public class MenuScreen extends AbstractScreen implements InputProcessor{
 	TextureRegion title;
+	TextureRegion startGame;
 	SpriteBatch batch;
 	float time = 0;
 	
@@ -20,8 +22,10 @@ public class MenuScreen extends AbstractScreen {
 	@Override
 	public void show () {
 		title = new TextureRegion(new Texture(Gdx.files.internal("data/Hero.png")), 0, 0, 480, 320);
+		startGame = new TextureRegion(new Texture(Gdx.files.internal("data/Start.png")), 0, 0, 240, 160 );
 		batch = new SpriteBatch();
 		batch.getProjectionMatrix().setToOrtho2D(0, 0, 480, 320);
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -29,14 +33,15 @@ public class MenuScreen extends AbstractScreen {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(title, 0, 0);
+		batch.draw(startGame, 40, 40);
 		batch.end();
 
 		time += delta;
 		if (time > 1) {
-			if (Gdx.input.isKeyPressed(Keys.ANY_KEY) || Gdx.input.justTouched()) {
+			if  (Gdx.input.getX() > 40 && Gdx.input.getX() < 280 && Gdx.input.getY() > 40 && Gdx.input.getY() < 200  ) {
 				game.setScreen(new GameScreen(game));
 			}
-		}
+		} 
 	}
 
 	@Override
@@ -44,5 +49,52 @@ public class MenuScreen extends AbstractScreen {
 		Gdx.app.debug("Roguish", "dispose main menu");
 		batch.dispose();
 		title.getTexture().dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
