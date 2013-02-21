62c1cbd5ba53608eaf7f05c19863fb560edde839
Curtis Giddings <curtis.giddings@gmail.com>
2013-02-20 23:50:24 -0800

    Fixed Tiles
    Fuck you

--- Roguish/src/com/me/Roguish/Model/Level.java
+++ Roguish/src/com/me/Roguish/Model/Level.java
@@ -1,169 +1,8 @@
 package com.me.Roguish.Model;
-/*
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.tiled.TileAtlas;
-import com.badlogic.gdx.graphics.g2d.tiled.TileMapRenderer;
-import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
-import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
-import com.badlogic.gdx.graphics.g2d.tiled.TiledObject;
-import com.badlogic.gdx.graphics.g2d.tiled.TiledObjectGroup;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
-//import com.badlogic.gdx.tests.utils.GdxTest;
-//import com.badlogic.gdx.tests.utils.OrthoCamController;
-
-/** @author David Fraska */ /*
-public class Level {
-
-	private static final boolean automove = false;
-
-	//private static final int[] layersList = {2, 3};
-
-	SpriteBatch spriteBatch;
-	BitmapFont font;
-
-	OrthographicCamera cam;
-	//OrthoCamController camController;
-	Vector3 camDirection = new Vector3(1, 1, 0);
-	Vector2 maxCamPosition = new Vector2(0, 0);
-
-	TileMapRenderer tileMapRenderer;
-	TiledMap map;
-	TileAtlas atlas;
-
-	//long startTime = System.nanoTime();
-	Vector3 tmp = new Vector3();
-
-	//@Override
-	public void render() {
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-
-		if (automove) {
-			updateCameraPosition();
-		}
-
-		cam.zoom = 0.9f;
-		cam.update();
-		// tileMapRenderer.getProjectionMatrix().set(cam.combined); //Not required when using tileMapRenderer.render(cam)
-		tileMapRenderer.render(cam);// , layersList);
-
-		spriteBatch.begin();
-		font.draw(spriteBatch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 20, 20);
-		font.draw(spriteBatch, "InitialCol, LastCol: " + tileMapRenderer.getInitialCol() + "," + tileMapRenderer.getLastCol(), 20,
-			40);
-		font.draw(spriteBatch, "InitialRow, LastRow: " + tileMapRenderer.getInitialRow() + "," + tileMapRenderer.getLastRow(), 20,
-			60);
-
-		tmp.set(0, 0, 0);
-		cam.unproject(tmp);
-		font.draw(spriteBatch, "Location: " + tmp.x + "," + tmp.y, 20, 80);
-		spriteBatch.end();
-	}
-
-	private void updateCameraPosition () {
-		cam.position.add(camDirection.tmp().mul(Gdx.graphics.getDeltaTime()).mul(5 * tileMapRenderer.getUnitsPerTileX()));
-
-		if (cam.position.x < 0) {
-			cam.position.x = 0;
-			camDirection.x = 1;
-		}
-		if (cam.position.x > maxCamPosition.x) {
-			cam.position.x = maxCamPosition.x;
-			camDirection.x = -1;
-		}
-		if (cam.position.y < 0) {
-			cam.position.y = 0;
-			camDirection.y = 1;
-		}
-		if (cam.position.y > maxCamPosition.y) {
-			cam.position.y = maxCamPosition.y;
-			camDirection.y = -1;
-		}
-	}
-
-	//@Override
-	public void create () {
-		int i;
-		long startTime, endTime;
-		font = new BitmapFont();
-		font.setColor(Color.RED);
-
-		spriteBatch = new SpriteBatch();
-
-		final String path = "data/level/";
-		final String mapname = "test";
-
-		FileHandle mapHandle = Gdx.files.internal(path + mapname + ".tmx");
-		FileHandle baseDir = Gdx.files.internal(path);
-
-		startTime = System.currentTimeMillis();
-		map = TiledLoader.createMap(mapHandle);
-		endTime = System.currentTimeMillis();
-		System.out.println("Loaded map in " + (endTime - startTime) + "mS");
-
-		atlas = new TileAtlas(map, baseDir);
-
-		int blockWidth = 15;
-		int blockHeight = 10;
-
-		startTime = System.currentTimeMillis();
-
-		tileMapRenderer = new TileMapRenderer(map, atlas, blockWidth, blockHeight, 16, 16);
-		endTime = System.currentTimeMillis();
-		System.out.println("Created cache in " + (endTime - startTime) + "mS");
-
-		for (TiledObjectGroup group : map.objectGroups) {
-			for (TiledObject object : group.objects) {
-				// TODO: Draw sprites where objects occur
-				System.out.println("Object " + object.name + " x,y = " + object.x + "," + object.y + " width,height = "
-					+ object.width + "," + object.height);
-			}
-		}
-
-		float aspectRatio = (float)Gdx.graphics.getWidth() / (float)Gdx.graphics.getHeight();
-		cam = new OrthographicCamera(100f * aspectRatio, 100f);
-
-		cam.position.set(tileMapRenderer.getMapWidthUnits() / 2, tileMapRenderer.getMapHeightUnits() / 2, 0);
-		//camController = new OrthoCamController(cam);
-		//Gdx.input.setInputProcessor(camController);
-
-		maxCamPosition.set(tileMapRenderer.getMapWidthUnits(), tileMapRenderer.getMapHeightUnits());
-	}
-
-	//@Override
-	public boolean needsGL20 () {
-		return false;
-	}
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import com.me.Roguish.Model.Entity;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.graphics.g2d.tiled.SimpleTileAtlas;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
 import com.badlogic.gdx.graphics.g2d.tiled.TileAtlas;
@@ -173,7 +12,7 @@ public class Level{
 	private Array<Entity> entities = new Array<Entity>();
 	
     public TiledMap map;
-    public TileAtlas atlas;
+    public SimpleTileAtlas atlas;
     
 	public Level(){
 		create();
@@ -185,7 +24,7 @@ public class Level{
 		System.out.println("In create");
 		map = TiledLoader.createMap(Gdx.files.internal("data/level/test.tmx"));
 		System.out.println("Tiles loaded");
-	    atlas = new TileAtlas(map, Gdx.files.internal("data/"));
+	    atlas = new SimpleTileAtlas(map, Gdx.files.internal("data/"));
 	    System.out.println("atlas made");
 	}
 
