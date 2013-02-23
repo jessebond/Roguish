e1bdd33b7d6a62f93242e818211f19e1fdc3ae6d
Jesse Bond <jjb315@lehigh.edu>
2013-02-23 18:07:40 -0500

    json stuff

--- Roguish/src/com/me/Roguish/Model/Card.java
+++ Roguish/src/com/me/Roguish/Model/Card.java
@@ -3,15 +3,16 @@ package com.me.Roguish.Model;
 import com.badlogic.gdx.utils.Json;
 
 public abstract class Card {
-	private Json json; 
+	//private Json json; 
 	
 	private String name;		// Unique flavor based card name
 	private int rarity;			// Rarity 
 
+	/*
 	private void loadData(String cardName){
 		json = new Json();
 		
-	}
+	}*/
 	
 	public String getName(){
 		return name;
