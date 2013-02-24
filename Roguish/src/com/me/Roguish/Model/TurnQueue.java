08c2ce7673c7ae54202c16217b45e25a5242ca11
Jesse Bond <jjb315@lehigh.edu>
2013-02-23 21:54:12 -0500

    added TurnQueue

---
+++ Roguish/src/com/me/Roguish/Model/TurnQueue.java
@@ -0,0 +1,12 @@
+package com.me.Roguish.Model;
+
+import com.badlogic.gdx.utils.Array;
+
+public class TurnQueue {
+	
+	Array<Entity> entities;
+	public TurnQueue(Array<Entity> ent){
+		this.entities = ent;
+	}
+	
+}
